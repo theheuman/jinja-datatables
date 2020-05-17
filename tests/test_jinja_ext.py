00ddf957af431d1121667991e3ca66dce4b5d44c
TheHeuman <jacob.heuman@gmail.com>
2020-05-17 16:57:41 -0400

    seperate different templates in test

--- tests/test_jinja_ext.py
+++ tests/test_jinja_ext.py
@@ -2,7 +2,47 @@
 import os
 from jinja2 import Environment, FileSystemLoader
 from jinja_datatables.jinja_extensions.datatableext import DatatableExt
-from jinja_datatables.datatable_classes import DatatableColumn, DatatableTable
+from jinja_datatables.datatable_classes import DatatableColumn, AjaxDatatable, JSArrayDatatable, HTMLDatatable
+
+
+def test_ajax(template):
+    columns = [
+        DatatableColumn("data_name", "Column Name", "filter_text"),
+    ]
+    endpoint = "/get_data"
+    table_view = AjaxDatatable(
+        columns,
+        None,
+        None,
+        endpoint,
+    )
+    return template.render(table_view=table_view)
+
+
+def test_js_array(template):
+    columns = [
+        DatatableColumn("data_name", "Column Name", "text"),
+    ]
+    js_array = "dataArray"
+    table_view = JSArrayDatatable(
+        columns,
+        None,
+        None,
+        js_array,
+    )
+    return template.render(table_view=table_view)
+
+
+def test_html(template):
+    columns = [
+        DatatableColumn("data_name", "Column Name", "filter_text"),
+    ]
+    table_view = HTMLDatatable(
+        columns,
+        None,
+        None,
+    )
+    return template.render(table_view=table_view)
 
 
 def main():
@@ -11,23 +51,9 @@ def main():
     env = Environment(loader=FileSystemLoader(template_dir), extensions=[DatatableExt])
     template = env.get_template("datatable_template.html")
 
-    columns = [
-        DatatableColumn("data_name", "Column Name", "filter_text"),
-    ]
-    endpoint = "/get_data"
-    rpp = 25
-    processing = True
-    selector = ".myDataTable"
-    table_view = DatatableTable(
-        columns,
-        endpoint,
-        rpp,
-        processing,
-        selector,
-    )
-    print(template.render(
-        table_view=table_view
-    ))
+    print(test_ajax(template))
+    # print(test_js_array(template))
+    # print(test_html(template))
 
 
 if __name__ == '__main__':
